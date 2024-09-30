332b72d3973ed9b6c7d6be1a5800e3f702b588c6
Git <petroklin@gmail.com>
2024-09-30 18:16:49 +0300

    Добавлен блок аутентификации пользователя + добавлена меню

--- project_k/project_k/urls.py
+++ project_k/project_k/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from report import views
+from django.urls import path, include
+from report.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name='index'),
-    path("create_dgu/", views.create_dgu, name='create_dgu'),
-    path("create_report/<int:id>/", views.create_report, name='create_report'),
-    path("edit_dgu/<int:id>/", views.edit_dgu, name='edit_dgu'),
-    path("delete_dgu/<int:id>/", views.delete_dgu, name='delete_dgu'),
+    path("", index, name='index'),
+    path("create_dgu/", create_dgu, name='create_dgu'),
+    path("create_report/<int:id>/", create_report, name='create_report'),
+    path("edit_dgu/<int:id>/", edit_dgu, name='edit_dgu'),
+    path("delete_dgu/<int:id>/", delete_dgu, name='delete_dgu'),
+    path('users/', include('users.urls', namespace="users")),
 ]
 
 admin.site.site_header = "Панель администрирования"
